c8e626b408b8c9efa44d9b82fd2f1c2c308fc894
github username <github email address>
2019-05-22 19:06:52 -0400

    Add politic and Reppit project

--- src/Projects.js
+++ src/Projects.js
@@ -4,6 +4,9 @@ import superman from './Superman-cutout.png';
 import './App.css';
 import ReactGA from 'react-ga'
 import { Typography } from '@material-ui/core'
+import { Grid , Paper} from '@material-ui/core'
+import politic from './politicScreenshot.png'
+import reppit from './reppitScreenshot.png'
 
 class Projects extends Component {
 
@@ -13,7 +16,20 @@ class Projects extends Component {
     ReactGA.initialize('UA-137802850-1');
     ReactGA.pageview('/projects');
     return (
-      <p>Projects</p>
+      <Grid container spacing={24}>
+        <Grid item xs={6}>
+          <Paper >politic</Paper>
+          <a href="https://politic.penchenski.com" target="blank">
+          <img src={politic} className="myImage" alt="Politic Screenshot"/>
+          </a>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper >Reppit</Paper>
+          <a href="https://reppit.penchenski.com" target="blank">
+          <img src={reppit} className="myImage" alt="Politic Screenshot"/>
+          </a>
+        </Grid>
+        </Grid>
     );
   }
 }
