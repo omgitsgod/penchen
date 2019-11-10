bde74a04a8f767a673a369af40d1ffc90b645bac
omgitsgod <omgitsgod@gmail.com>
2019-11-10 15:47:34 -0500

    Change from a class component to a functional component

--- src/Resume.js
+++ src/Resume.js
@@ -1,40 +1,31 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { useEffect } from 'react';
+import './css/Resume.css';
 import ReactGA from 'react-ga'
-import { Typography } from '@material-ui/core'
 import  { Document, Page } from 'react-pdf';
 import resume from './resume.pdf'
-import { Grid , Paper} from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 
-class Resume extends Component {
-
-  state = {
-    numPages: 1
-  }
-
-  onDocumentLoadSuccess = ({ numPages }) => {
-    this.setState({ numPages });
-  }
-
-  render() {
+function Resume () {
+  useEffect(() => {
     ReactGA.initialize('UA-137802850-1');
     ReactGA.pageview('/resume');
-    return (
-      <div>
+  }, []);
+
+  return (
+    <div>
       <br />
       <br />
-        <Grid container spacing={24}>
-         <Grid item xs={12}>
+      <Grid container spacing={24}>
+       <Grid item xs={12}>
          <Document file={resume}
           className="pdf"
           noData={<h4>Try again later</h4>}>
            <Page pageNumber={1} />
          </Document>
-         </Grid>
-         </Grid>
-       </div>
-    );
-  }
+       </Grid>
+      </Grid>
+    </div>
+  );
 }
 
 export default Resume;
