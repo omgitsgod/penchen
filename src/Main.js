a804e87155623fab38689dce677b210bbcffe545
omgitsgod <omgitsgod@gmail.com>
2019-11-10 15:23:45 -0500

    Change from a class component to a functional component

--- src/Main.js
+++ src/Main.js
@@ -1,62 +1,60 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import superman from './imgs/Superman-cutout.png';
-import './App.css';
-import ReactGA from 'react-ga'
-import { Typography } from '@material-ui/core'
-import Icon from '@mdi/react'
-import { mdiLinkedinBox, mdiGithubCircle, mdiMedium, mdiEmailOutline, mdiStackOverflow } from '@mdi/js'
+import './css/Main.css';
+import ReactGA from 'react-ga';
+import { Typography } from '@material-ui/core';
+import Icon from '@mdi/react';
+import { mdiLinkedinBox, mdiGithubCircle, mdiEmailOutline } from '@mdi/js';
 
-class Main extends Component {
-
-
-
-  render() {
+function Main () {
+  useEffect(() => {
     ReactGA.initialize('UA-137802850-1');
     ReactGA.pageview('/homepage');
-    return (
-      <div className="App">
+  }, []);
 
-        <header className="App-header">
+  return (
+    <div className="Main">
 
-          <img src={superman} className="App-logo" alt="logo" />
+      <div className="Main-div">
 
-          <br/>
-          <Typography variant='display3' align='center' gutterBottom>
+        <img src={superman} className="superman" alt="Superman" />
+
+        <br/>
+        <Typography variant='display3' align='center' gutterBottom>
           Hello there, I'm Ryan!
-          </Typography>
-          <br/>
-          <Typography variant='p' align='center' gutterBottom>
+        </Typography>
+        <br/>
+        <Typography variant='p' align='center' gutterBottom>
           Full-Stack Software Engineer | Physics Enthusiast | Lover Of Puzzles
-          </Typography>
-          <br />
-          <br />
-          <div className="center">
+        </Typography>
+        <br />
+        <br />
+        <div className="center">
           <a href="https://www.linkedin.com/in/penchenski/" target="blank">
-          <Icon path={mdiLinkedinBox}
-            size={3}
-            className='icon'
-            color="white"
-            />
+            <Icon path={mdiLinkedinBox}
+              size={3}
+              className='icon'
+              color="white"
+              />
           </a>
           <a href="https://github.com/omgitsgod/" target="blank">
-          <Icon path={mdiGithubCircle}
-            size={3}
-            className='icon'
-            color="white"
-            />
+            <Icon path={mdiGithubCircle}
+              size={3}
+              className='icon'
+              color="white"
+              />
           </a>
           <a href="mailto:rpenchenski@gmail.com" target="blank">
-          <Icon path={mdiEmailOutline}
-            size={3}
-            className='icon'
-            color="white"
-            />
+            <Icon path={mdiEmailOutline}
+              size={3}
+              className='icon'
+              color="white"
+              />
           </a>
-          </div>
-        </header>
+        </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Main;
