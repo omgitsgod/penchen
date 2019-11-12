a7c4cbe7f7a453dd422f53e2a6d9a81730ba98d9
omgitsgod <omgitsgod@gmail.com>
2019-11-12 14:44:14 -0500

    Change body2 to body1

--- src/MediaCard.js
+++ src/MediaCard.js
@@ -37,7 +37,7 @@ export default function MediaCard(props) {
           <Typography gutterBottom variant="h5" component="h2">
             {props.name}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography variant="body1" color="textSecondary" component="p">
             {props.text}
           </Typography>
         </CardContent>
