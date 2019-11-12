717f77dec558af7a75653f423a89865b6dcdc4a8
omgitsgod <omgitsgod@gmail.com>
2019-11-12 13:54:25 -0500

    Clean up

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,18 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom'
 import './css/NavBar.css';
-import Icon from '@mdi/react'
-import {mdiMenuOpen} from '@mdi/js'
 
 function NavBar() {
 
   return (
 
-    <div className="navbar" >
-      <div className="navbar-div">
-        <Link to='/' className="navy"><p>Home</p></Link>
-        <Link to='/projects' className="navy"><p>Projects</p></Link>
-        <Link to='/resume' className="navy"><p>Resume</p></Link>
+    <div className='navbar'>
+      <div className='navbar-div'>
+        <Link to='/' className='navy'><p>Home</p></Link>
+        <Link to='/projects' className='navy'><p>Projects</p></Link>
+        <Link to='/resume' className='navy'><p>Resume</p></Link>
       </div>
     </div>
   )
