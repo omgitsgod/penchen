377027f14edbae63000960870b12b751ba72c55a
omgitsgod <omgitsgod@gmail.com>
2019-11-12 14:46:10 -0500

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import React, { Component, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import './css/App.css';
-import ReactGA from 'react-ga';
 import stars from './stars.mp4';
-import { Route, Link, Switch  } from 'react-router-dom';
-import { Tab, Tabs, Typography} from '@material-ui/core';
+import { Route, Switch  } from 'react-router-dom';
 import NavBar from './NavBar'
 import Main from './Main';
 import Projects from './Projects';
@@ -13,19 +11,19 @@ function App() {
   return (
     <div>
     <Route path="/" render={({location}) => (
-    <Fragment>
-    <video className="myVideo" loop autoPlay muted>
-     <source src={stars} type='video/mp4' />
-     <source src={stars} type='video/ogg' />
-     Your browser does not support the video tag.
-   </video>
-    <NavBar />
-    <Switch>
-    <Route exact path="/" component={Main} />
-    <Route exact path="/projects" component={Projects} />
-    <Route path="/resume" component={Resume} />
-    </Switch>
-    </Fragment>
+      <Fragment>
+        <video className="myVideo" loop autoPlay muted>
+         <source src={stars} type='video/mp4' />
+         <source src={stars} type='video/ogg' />
+         Your browser does not support the video tag.
+       </video>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route exact path="/projects" component={Projects} />
+          <Route path="/resume" component={Resume} />
+        </Switch>
+      </Fragment>
     )}
     />
     </div>
