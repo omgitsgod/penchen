8033daf134475512caa9d30359d1091d76c7b3dd
omgitsgod <omgitsgod@gmail.com>
2019-11-12 21:57:34 -0500

    Change 'Link' to 'link' and 'http' to 'https'

--- src/Projects.js
+++ src/Projects.js
@@ -32,42 +32,42 @@ function Projects() {
   justify="center">
           <Grid item md={6} sm={12}>
             <div data-wow-delay="1s">
-            <Card className="project" name="Vinyasa Flow" img={vinyasa} tech='#React.js #Node.js #MongoDB' Link="https://vinyasa.penchenski.com" text="Plan out your yoga classes" git="https://github.com/omgitsgod/vinyasa-front"/>
+            <Card className="project" name="Vinyasa Flow" img={vinyasa} tech='#React.js #Node.js #MongoDB' link="https://vinyasa.penchenski.com" text="Plan out your yoga classes" git="https://github.com/omgitsgod/vinyasa-front"/>
             </div>
           </Grid>
           <Grid item md={6} sm={12}>
             <div data-wow-delay="1.25s">
-            <Card className="project" name="PictoPal" img={pictopal} tech='' Link="https://pictopal.penchenski.com" text="Drawing app" git="https://github.com/omgitsgod/pictopal-front"/>
+            <Card className="project" name="PictoPal" img={pictopal} tech='' link="https://pictopal.penchenski.com" text="Drawing app" git="https://github.com/omgitsgod/pictopal-front"/>
             </div>
           </Grid>
           <Grid item md={6} sm={12}>
             <div data-wow-delay="1.5s">
-            <Card className="project" name="fluxunit" img={fluxunit} tech='' Link="https://www.npmjs.com/package/fluxunit" text="A simple way to convert units of measurement" git="https://github.com/omgitsgod/fluxunit"/>
+            <Card className="project" name="fluxunit" img={fluxunit} tech='' link="https://www.npmjs.com/package/fluxunit" text="A simple way to convert units of measurement" git="https://github.com/omgitsgod/fluxunit"/>
             </div>
           </Grid>
           <Grid item md={6} sm={12}>
             <div data-wow-delay="1.75s">
-            <Card className="project" name="Equanimity Yoga" img={equanimity} tech='' Link="https://equanimityyoganyc.com" text="Homepage for Equanimity Yoga" git="https://github.com/omgitsgod/yoga_2019"/>
+            <Card className="project" name="Equanimity Yoga" img={equanimity} tech='' link="https://equanimityyoganyc.com" text="Homepage for Equanimity Yoga" git="https://github.com/omgitsgod/yoga_2019"/>
             </div>
           </Grid>
           <Grid item md={6} sm={12}>
             <div data-wow-delay="1s">
-            <Card className="project" name="politic" img={politic} tech='' Link="http://politic.penchenski.com" text="Find out whats going on in politics" git="https://github.com/omgitsgod/politic-heroku"/>
+            <Card className="project" name="politic" img={politic} tech='' link="https://politic.penchenski.com" text="Find out whats going on in politics" git="https://github.com/omgitsgod/politic-heroku"/>
             </div>
           </Grid>
           <Grid item md={6} sm={12}>
             <div data-wow-delay="1.25s">
-            <Card className="project" name="Reppit" img={reppit} tech='' Link="http://reppit.penchenski.com" text="Track workouts" git="https://github.com/omgitsgod/reppit-frontend"/>
+            <Card className="project" name="Reppit" img={reppit} tech='' link="https://reppit.penchenski.com" text="Track workouts" git="https://github.com/omgitsgod/reppit-frontend"/>
             </div>
           </Grid>
           <Grid item md={6} sm={12}>
             <div data-wow-delay="1.5s">
-            <Card className="project" name="Metrognome" img={metrognome} tech='' Link="http://metrognome.penchenski.com" text="Simple React metronome with custom timer" git="https://github.com/omgitsgod/metrognome"/>
+            <Card className="project" name="Metrognome" img={metrognome} tech='' link="https://metrognome.penchenski.com" text="Simple React metronome with custom timer" git="https://github.com/omgitsgod/metrognome"/>
             </div>
           </Grid>
           <Grid item md={6} sm={12}>
             <div data-wow-delay="1.75s">
-            <Card className="project" name="Feelslike Forecast" img={forecast} tech='' Link="http://forecast.penchenski.com" text="Simple weather app with forecast support by day" git="https://github.com/omgitsgod/feelslike-forecast"/>
+            <Card className="project" name="Feelslike Forecast" img={forecast} tech='' link="https://forecast.penchenski.com" text="Simple weather app with forecast support by day" git="https://github.com/omgitsgod/feelslike-forecast"/>
             </div>
           </Grid>
         </Grid>
