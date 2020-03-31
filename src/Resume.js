f465232cd675c6f080d0cbc4dafb36bf1faa8e6f
omgitsgod <omgitsgod@gmail.com>
2020-03-31 12:53:13 -0400

    Change location of resume to public folder and add link to resume on click

--- src/Resume.js
+++ src/Resume.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import './css/Resume.css';
 import ReactGA from 'react-ga'
 import  { Document, Page } from 'react-pdf';
-import resume from './resume.pdf'
 import { Grid } from '@material-ui/core'
 
 function Resume () {
@@ -11,18 +10,22 @@ function Resume () {
     ReactGA.pageview('/resume');
   }, []);
 
+  const resume = process.env.PUBLIC_URL + 'resume.pdf'
+
   return (
     <div className='resume'>
       <br />
       <br />
       <Grid container spacing={24}>
        <Grid item xs={12}>
-         <Document file={resume}
-          className="pdf"
-          width= '5%'
-          noData={<h4>Try again later</h4>}>
-           <Page pageNumber={1} />
-         </Document>
+        <a href={resume} target='blank'>
+          <Document file={resume}
+            className="pdf"
+            width= '5%'
+            noData={<h4>Try again later</h4>}>
+            <Page pageNumber={1} />
+          </Document>
+        </a>
        </Grid>
       </Grid>
     </div>
