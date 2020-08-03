a96c95d5740e843b2c0194f52225957ae7bc224f
Ryan <omgitsgod@gmail.com>
2020-08-02 22:30:40 -0400

    Change structure of App

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React, { Fragment } from 'react';
-import './css/App.css';
-import stars from './stars.mp4';
-import { Route, Switch  } from 'react-router-dom';
-import NavBar from './NavBar'
-import Main from './Main';
-import Projects from './Projects';
-import Resume from './Resume';
+import React, { useState, useEffect } from 'react';
+import ReactGA from 'react-ga';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Portfolio from './Components/Portfolio';
 
 function App() {
+  const [data, setData] = useState({});
+  const getData = async () => {
+    const fetchedData = await fetch('/resumeData.json').then((r) => r.json());
+    setData(fetchedData);
+    console.log(fetchedData)
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div>
-    <Route path="/" render={({location}) => (
-      <Fragment>
-        <video className="myVideo" loop autoPlay muted>
-         <source src={stars} type='video/mp4' />
-         <source src={stars} type='video/ogg' />
-         Your browser does not support the video tag.
-       </video>
-        <NavBar />
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route exact path="/projects" component={Projects} />
-          <Route path="/resume" component={Resume} />
-        </Switch>
-      </Fragment>
-    )}
-    />
+    <div className='App'>
+      {data.main ?
+      <>
+      <Header data={data.main} />
+      <About data={data.main} />
+      <Portfolio data={data.portfolio} />
+      <Contact data={data.main} />
+      <Footer data={data.main} />
+      </>
+    : null
+  }
     </div>
   );
 }
