6ee782f714de8de4ab9598c4f1331b4fd606f92e
Ryan <omgitsgod@gmail.com>
2020-08-02 22:30:19 -0400

    Add css imports

--- src/index.js
+++ src/index.js
@@ -2,38 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router } from 'react-router-dom'
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
-import { grey, blue, red, green} from '@material-ui/core/colors'
-import { CssBaseline } from '@material-ui/core'
 import * as serviceWorker from './serviceWorker';
-import dotenv from 'dotenv'
-import { pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-const theme = createMuiTheme({
-  palette: {
-    primary: grey,
-    secondary: blue,
-    error: red,
-    success: green,
-    type: 'dark'
-  },
-
-
-})
-
-dotenv.config()
-
-ReactDOM.render(
-  <Router>
-  <MuiThemeProvider theme={theme}>
-  <CssBaseline />
-  <App />
-  </MuiThemeProvider>
-  </Router>, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
